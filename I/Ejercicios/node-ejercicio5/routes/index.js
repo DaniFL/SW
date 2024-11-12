b8117f2e0ee4a46a3c4ad21df251fb96bf521ad1
DaniFL <danielfernandezlopez75@gmail.com>
2024-11-12 13:56:24 +0100

    Ejercicio 5 - NodeJS | error en pantalla index - css

--- I/Ejercicios/node-ejercicio5/routes/index.js
+++ I/Ejercicios/node-ejercicio5/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Ferrari Retro Club',
+    intro: 'Welcome to Ferrari Retro Club',
+    button: 'Login'
+   });
 });
 
 module.exports = router;
--- I/Ejercicios/node-ejercicio5/routes/login.js
+++ I/Ejercicios/node-ejercicio5/routes/login.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("login", { title: "Node-Ejs5" });
+  res.render("login", { 
+    title: "FRC/Login",
+    user_wlc: "Welcome",
+    user_mssg: "sign up to continue",
+    submit: "Submit →",
+   });
 });
 
 module.exports = router;
